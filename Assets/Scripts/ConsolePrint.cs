6c0d38748f421686a6fdd0c45473d6e6de5745e2
HuyLuong <ngochuybu@gmail.com>
2024-08-30 20:43:43 +1000

    80% D

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    private int i = 3;
+
+    void Start()
+    {
+        i++; 
+    }
+
+    void Update()
+    {
+        
+        Debug.Log(gameObject.name + ":" + i);
+        i++;
+    }
+}
